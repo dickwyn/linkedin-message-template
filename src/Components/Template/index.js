e5b97ffde8257bcb5864acad54cd7d76d084755b
dickwyn <dickwyn@outlook.com>
2019-02-02 16:11:35 -0700

    cleanup and add states to template component

--- src/Components/Template/index.js
+++ src/Components/Template/index.js
@@ -2,106 +2,86 @@ import React, { Component } from 'react';
 import '../../styles/template.scss';
 
 class Template extends Component {
+  state = {
+    template: 'Default',
+    name: 'dickwyn',
+    company: 'testorange',
+    job: 'full stack developer roles',
+    signature: true,
+  };
+
+  onChange = () => {
+    this.setState({});
+  };
+
   render() {
+    const { template, name, company, job, signature } = this.state;
+
     return (
       <div className="template">
-        <div className="title">Default template</div>
-        <div className="message">
-          <div className="card">
-            <div className="card-body">
-              <p className="lead" id="copyMessage">
-                Hi <span id="name" />,
-                <br />
-                <br />I came across your profile through the alumni search. I
-                will be graduating ASU this December with a CS degree. I'm
-                looking to apply for <span id="job" /> within{' '}
-                <span id="company" />. I would like to have a chat with you to
-                get some pointers when applying for a role at{' '}
-                <span id="company" />.<span id="signature" />
-                <span id="txtAge">
-                  <br />
-                  <br />
-                  Go Devils!
-                </span>
-              </p>
-            </div>
-          </div>
+        <div className="title">
+          <h1>{template} template</h1>
         </div>
-        <div className="inputs">
-          <div className="select">
-            <label htmlFor="templateName" className="col-sm-2 col-form-label">
-              Template
-            </label>
+        <div className="message">
+          Hi {name}, I came across your profile through the alumni search. I
+          will be graduating ASU this December with a CS degree. I'm looking to
+          apply for {job}within {company}. I would like to have a chat with you
+          to get some pointers when applying for a role at {company}. Go Devils!
+        </div>
+        <div className="form">
+          <div className="template-select">
+            <label className="form-label">Template</label>
             <div className="col-sm-10" id="templateName">
               <select className="form-control">
                 <option>Default Template</option>
-                <option>Template #1</option>
-                <option>Templete #2</option>
               </select>
             </div>
           </div>
           <div className="name">
-            <label htmlFor="personName" className="col-sm-2 col-form-label">
-              Name
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="text"
-                className="form-control"
-                id="personName"
-                placeholder="John"
-              />
-            </div>
+            <label className="form-label">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="personName"
+              placeholder="John"
+            />
           </div>
           <div className="company">
-            <label htmlFor="companyName" className="col-sm-2 col-form-label">
-              Company
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="text"
-                className="form-control"
-                id="companyName"
-                placeholder="Google"
-              />
-            </div>
+            <label className="form-label">Company</label>
+            <input
+              type="text"
+              className="form-control"
+              id="companyName"
+              placeholder="Google"
+            />
           </div>
           <div className="job">
-            <label htmlFor="jobName" className="col-sm-2 col-form-label">
-              Job
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="text"
-                className="form-control"
-                id="jobName"
-                placeholder="Full Stack developer roles"
-              />
-            </div>
+            <label className="form-label">Job</label>
+            <input
+              type="text"
+              className="form-control"
+              id="jobName"
+              placeholder="Full Stack developer roles"
+            />
           </div>
           <div className="signature">
-            <label htmlFor="jobName" className="col-sm-2 col-form-label" />
-            <div className="col-sm-10">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="signatureName"
-                  checked
-                />
-                <label className="form-check-label" htmlFor="defaultCheck1">
-                  Signature
-                </label>
-              </div>
-            </div>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="signatureName"
+              checked
+            />
+            <label className="form-check-label">Signature</label>
           </div>
-          <button
-            type="button"
-            className="btn btn-outline-primary btn-block"
-            data-clipboard-target="#copyMessage"
-          >
-            Copy Text
-          </button>
+          <div className="copy-button">
+            <button
+              type="button"
+              className="btn btn-outline-primary btn-block"
+              data-clipboard-target="#copyMessage"
+            >
+              Copy Text
+            </button>
+          </div>
         </div>
       </div>
     );
