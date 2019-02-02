f073ccf1524b19f09dbc8c4cf2f5da1b59a27489
dickwyn <dickwyn@outlook.com>
2019-02-02 11:58:46 -0700

    restore functionality of application

--- script.js
+++ script.js
@@ -13,35 +13,39 @@
 //    $("#company").text(value);
 //}).keyup();
 
-$('form').keyup(function () {
-    var nameVar = $(this).find('#personName').val();
-    var companyVar = $(this).find('#companyName').val();
-    var jobVar = $(this).find('#jobName').val();
-    $("#name").text(nameVar);
+$('form')
+  .keyup(function() {
+    var nameVar = $(this)
+      .find('#personName')
+      .val();
+    var companyVar = $(this)
+      .find('#companyName')
+      .val();
+    var jobVar = $(this)
+      .find('#jobName')
+      .val();
+    $('#name').text(nameVar);
     $('span[id="company"]').text(companyVar);
     $('span[id="job"]').text(jobVar);
-}).keyup();
+  })
+  .keyup();
 
-$(function() {
-	$('select').change(function() {
-    $('.card-body p').hide();
-    console.log($(this).val());
-  	$('p.t-' + $(this).val()).show();
-  });
-});
-
-$('#templateChoice').change(function () {
-    var value = $(this).find(' :selected').text();
-    $(".template-name").text(value);
-}).change();
+$('#templateName')
+  .change(function() {
+    var value = $(this)
+      .find(' :selected')
+      .text();
+    $('.template').text(value);
+  })
+  .change();
 
 $('#signatureName').click(function() {
-    $("#txtAge").toggle(this.checked);
+  $('#txtAge').toggle(this.checked);
 });
 
 // clipboard js functionality
 new ClipboardJS('.btn');
 
 // check char length
-var length = $(".lead").text().length;
+var length = $('.lead').text().length;
 console.log(length);
