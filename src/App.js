bb6f61ab5887d782d466c1dbeadb5fcd23f6d11a
dickwyn <dickwyn@outlook.com>
2019-02-02 23:42:02 -0700

    update component js names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles/App.scss';
-import Template from './Components/Template';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Template from './Components/Template/Template';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
