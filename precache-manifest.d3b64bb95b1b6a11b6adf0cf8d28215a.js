ff0dd8011417f831f6d54b848a16c022fcae7e6c
dickwyn <dickwyn@outlook.com>
2019-08-04 16:58:52 -0700

    Updates

---
+++ precache-manifest.d3b64bb95b1b6a11b6adf0cf8d28215a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8d81a484a07facc36f0d8b3253be4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a45950ab17eefe84eec",
+    "url": "/static/css/main.80d13a0c.chunk.css"
+  },
+  {
+    "revision": "9299a6385c41ace3405e",
+    "url": "/static/js/2.1d082056.chunk.js"
+  },
+  {
+    "revision": "2a45950ab17eefe84eec",
+    "url": "/static/js/main.272eaee9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
